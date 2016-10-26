3c5eddf163c8db9e30fe8533bc30ff13815f5a61
Thomas Aubin <thomas@sqreen.io>
2016-10-26 13:28:31 +0200

    improve categorization

--- caepainvestigatio/scan/categorization.py
+++ caepainvestigatio/scan/categorization.py
@@ -1,4 +1,6 @@
 """ try to categorize the onion """
+import re
+
 from caepainvestigatio.logging_conf import initLogging
 from caepainvestigatio.ORM.categories import Category
 from langdetect import detect
@@ -20,11 +22,15 @@ def search_category(onion_info):
 
     category_find = []
     for category in Category.objects():
+        count_w = 0
         for expression in category.expressions:
             # find expression in onion website sources
-            if expression in onion_info.snapshot:
-                log.debug("detect category %s for onion %s", category.category, onion_info.hiddenService)
-                if category not in category_find:
+            count = sum(1 for _ in re.finditer(r'\b%s\b' % re.escape(expression), onion_info.snapshot))
+            if count > 0:
+                count_w += count
+                if count_w > 2 and category not in category_find:
+                    log.debug("detect category %s for onion %s", category.category, onion_info.hiddenService)
                     category_find.append(category)
+                    break
 
     return category_find
