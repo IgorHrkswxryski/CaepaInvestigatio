8cafa4c30eab2bf5bbdccd497dd735a2f77d9a32
Thomas Aubin <thomas@sqreen.io>
2016-10-26 13:10:37 +0200

    get link already find by onionscan

--- caepainvestigatio/scan/search_onion.py
+++ caepainvestigatio/scan/search_onion.py
@@ -11,13 +11,17 @@ def search_onion(onion_info):
 
     links = []
     regex = ur"((?:[a-z0-9]+\.)*[a-z0-9]{16}.onion)"
-    code = onion_info.snapshot
 
-    finds = re.findall(regex, code)
-    if finds != []:
-        for elem in finds:
-            if elem not in links and elem != onion_info.hiddenService:
-                links.append(elem)
+    # get link already find by onionscan
+    for link in onion_info.linkedSites:
+        if re.match(regex, link):
+            links.append(link)
+
+    # find other onion link in snapshot
+    code = onion_info.snapshot
+    for elem in re.findall(regex, code):
+        if elem not in links and elem != onion_info.hiddenService:
+            links.append(elem)
 
     log.debug("Links find to %s", links)
     return links
